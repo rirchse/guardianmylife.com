f0038b353b0bd41d241fdac75db0cb0571be8fb1
rirchse <rirchse@gmail.com>
2025-08-04 10:26:35 +0600

    Session updated

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Auth;
+use Session;
 
 class LoginController extends Controller
 {
