7de458230034a09f0d0a8f5938a02f0228dc4433
rirchse <rirchse@gmail.com>
2025-08-13 00:23:27 +0600

    User photo upload avatar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -92,7 +92,7 @@ $user = Auth::user();
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{$user->photo? $user->photo : '/img/avater.png'}}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{$user->photo? $user->photo : '/img/avatar.png'}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">{{Auth::user()->name}}</a>
